75db0bed38d3fa8352bf69c7e1417e474b384248
Rafaela Tieri <rafaelaiwamoto302@gmail.com>
2024-09-03 16:18:19 -0300

    feat: Criando rotas, imports e exports de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 // import { Home } from "./pages/vaga/Home";
 
 import { Login } from "./pages/usuario/Login/login";
+import { Cadastro } from "./pages/usuario/Login/cadastro";
 
 export const App = () => {
     return (
         <>
             <Login />
+            <Cadastro />
         </>
     );
 };
---
+++ src/pages/usuario/Cadastro/cadastro.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../../../assets/logoHorizontal.png"
+import "./cadastro.css";
+
+export const Cadastro = () => { 
+return (
+    <div></div>
+
+    )
+}
