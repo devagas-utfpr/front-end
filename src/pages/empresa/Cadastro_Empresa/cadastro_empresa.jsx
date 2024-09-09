8db8db81a3fa74bffe54a6ff0da2312c6663c551
Rafaela Tieri <rafaelaiwamoto302@gmail.com>
2024-09-09 08:10:54 -0300

    feat: Campos do form

--- src/pages/empresa/Cadastro_Empresa/cadastro_empresa.jsx
+++ src/pages/empresa/Cadastro_Empresa/cadastro_empresa.jsx
@@ -8,6 +8,21 @@ export const CadastrarEmpresa = () => {
       <img src={logo} alt="Logo" className="logo" />
       <h2>Cadastre sua Empresa</h2>
       <form>
+      <div className="form-group">
+          <input type="text" placeholder="Nome" />
+        </div>
+        <div className="form-group">
+            <input type="email" placeholder="Email" />
+          </div>
+        <div className="form-row">
+          <div className="form-group">
+            <input type="password" placeholder="Senha" />
+          </div>
+          <div className="form-group">
+            <input type="password" placeholder="Confirmação de senha" />
+          </div>
+        </div>
+        <button type="submit">Cadastrar</button>
       </form>
     </div>
   );
