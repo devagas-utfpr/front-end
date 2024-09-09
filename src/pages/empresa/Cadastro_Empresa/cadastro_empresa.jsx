b8b6b344dc5b367a90a3e31182402417a548171e
Rafaela Tieri <rafaelaiwamoto302@gmail.com>
2024-09-09 08:08:56 -0300

    feat: Form e título

--- src/pages/empresa/Cadastro_Empresa/cadastro_empresa.jsx
+++ src/pages/empresa/Cadastro_Empresa/cadastro_empresa.jsx
@@ -4,8 +4,11 @@ import "./cadastro_empresa.css";
 
 export const CadastrarEmpresa = () => { 
   return (
-    <div>
-        
+    <div className="empresa-container">
+      <img src={logo} alt="Logo" className="logo" />
+      <h2>Cadastre sua Empresa</h2>
+      <form>
+      </form>
     </div>
   );
 };
