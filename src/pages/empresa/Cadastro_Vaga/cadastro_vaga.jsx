0960c13d52a1cebc61ab00261cc2e71f9a749e7a
Rafaela Tieri <rafaelaiwamoto302@gmail.com>
2024-09-09 23:45:01 -0300

    feat: Campos de título, empresa e cargo

--- src/pages/empresa/Cadastro_Vaga/cadastro_vaga.jsx
+++ src/pages/empresa/Cadastro_Vaga/cadastro_vaga.jsx
@@ -2,12 +2,32 @@ import React, { useState } from "react";
 import "./cadastro_vaga.css";
 
 export const CadastrarVaga = () => {
-
+    
   return (
     <div className="vaga-container">
     <div className="vaga-titulo">Cadastre uma vaga</div>
     <form>
-      
+      <div className="form-vaga-group">
+        <input
+          type="text"
+          className="vaga-input"
+          placeholder="Título"
+        />
+      </div>
+      <div className="form-vaga-group">
+        <input
+          type="text"
+          className="vaga-input"
+          placeholder="Empresa"
+        />
+      </div>
+        <div className="form-vaga-group">
+          <input
+            type="text"
+            className="vaga-input"
+            placeholder="Cargo"
+          />
+        </div>
     </form>
   </div>
   );
