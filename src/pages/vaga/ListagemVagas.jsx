c28fc38809d74d8f22e94b567c2f0a8603a00969
Eduardo Riki <eduardomatushita1@gmail.com>
2024-09-10 04:36:51 -0300

    fix: Correções gerais

---
+++ src/pages/vaga/ListagemVagas.jsx
@@ -0,0 +1,168 @@
+import { Header } from "../../shared/components";
+
+export const ListagemVagas = () => {
+    return (
+        <>
+            <Header />
+            <div class="bg-default md:px-10 px-4 py-12 font-[sans-serif]">
+                <div class="max-w-5xl max-lg:max-w-3xl max-sm:max-w-sm mx-auto">
+                    <h2 class="text-3xl font-extrabold text-gray-800 mb-8">
+                        Latest Blog Posts
+                    </h2>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-sm:gap-8">
+                        <div class="bg-white rounded overflow-hidden">
+                            <img
+                                src="https://readymadeui.com/cardImg.webp"
+                                alt="Blog Post 1"
+                                class="w-full h-52 object-cover"
+                            />
+                            <div class="p-6">
+                                <h3 class="text-lg font-bold text-gray-800 mb-3">
+                                    Lorem Ipsum Dolor
+                                </h3>
+                                <p class="text-gray-500 text-sm">
+                                    Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore...
+                                </p>
+                                <p class="text-orange-500 text-[13px] font-semibold mt-4">
+                                    08 April 2024
+                                </p>
+                                <a
+                                    href="javascript:void(0);"
+                                    class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div class="bg-white rounded overflow-hidden">
+                            <img
+                                src="https://readymadeui.com/hotel-img.webp"
+                                alt="Blog Post 2"
+                                class="w-full h-52 object-cover"
+                            />
+                            <div class="p-6">
+                                <h3 class="text-lg font-bold text-gray-800 mb-3">
+                                    Consectetur Adipiscing
+                                </h3>
+                                <p class="text-gray-500 text-sm">
+                                    Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore...
+                                </p>
+                                <p class="text-orange-500 text-[13px] font-semibold mt-4">
+                                    08 April 2024
+                                </p>
+                                <a
+                                    href="javascript:void(0);"
+                                    class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div class="bg-white rounded overflow-hidden">
+                            <img
+                                src="https://readymadeui.com/team-image.webp"
+                                alt="Blog Post 3"
+                                class="w-full h-52 object-cover"
+                            />
+                            <div class="p-6">
+                                <h3 class="text-lg font-bold text-gray-800 mb-3">
+                                    Lorem Ipsum Sit Amet
+                                </h3>
+                                <p class="text-gray-500 text-sm">
+                                    Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore...
+                                </p>
+                                <p class="text-orange-500 text-[13px] font-semibold mt-4">
+                                    08 April 2024
+                                </p>
+                                <a
+                                    href="javascript:void(0);"
+                                    class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div class="bg-white rounded overflow-hidden">
+                            <img
+                                src="https://readymadeui.com/prediction.webp"
+                                alt="Blog Post 3"
+                                class="w-full h-52 object-cover"
+                            />
+                            <div class="p-6">
+                                <h3 class="text-lg font-bold text-gray-800 mb-3">
+                                    Lorem Ipsum Sit Amet
+                                </h3>
+                                <p class="text-gray-500 text-sm">
+                                    Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore...
+                                </p>
+                                <p class="text-orange-500 text-[13px] font-semibold mt-4">
+                                    08 April 2024
+                                </p>
+                                <a
+                                    href="javascript:void(0);"
+                                    class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div class="bg-white rounded overflow-hidden">
+                            <img
+                                src="https://readymadeui.com/hacks-watch.webp"
+                                alt="Blog Post 3"
+                                class="w-full h-52 object-cover"
+                            />
+                            <div class="p-6">
+                                <h3 class="text-lg font-bold text-gray-800 mb-3">
+                                    Lorem Ipsum Sit Amet
+                                </h3>
+                                <p class="text-gray-500 text-sm">
+                                    Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore...
+                                </p>
+                                <p class="text-orange-500 text-[13px] font-semibold mt-4">
+                                    08 April 2024
+                                </p>
+                                <a
+                                    href="javascript:void(0);"
+                                    class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div class="bg-white rounded overflow-hidden">
+                            <img
+                                src="https://readymadeui.com/Imagination.webp"
+                                alt="Blog Post 3"
+                                class="w-full h-52 object-cover"
+                            />
+                            <div class="p-6">
+                                <h3 class="text-lg font-bold text-gray-800 mb-3">
+                                    Lorem Ipsum Sit Amet
+                                </h3>
+                                <p class="text-gray-500 text-sm">
+                                    Duis aute irure dolor in reprehenderit in
+                                    voluptate velit esse cillum dolore...
+                                </p>
+                                <p class="text-orange-500 text-[13px] font-semibold mt-4">
+                                    08 April 2024
+                                </p>
+                                <a
+                                    href="javascript:void(0);"
+                                    class="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
+                                >
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
