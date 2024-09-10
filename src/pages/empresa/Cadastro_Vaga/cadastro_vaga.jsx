c94c0787de3758c323f262dcbab03c8db1104209
Rafaela Tieri <rafaelaiwamoto302@gmail.com>
2024-09-09 23:47:25 -0300

    feat: Campo de descrição de vaga

--- src/pages/empresa/Cadastro_Vaga/cadastro_vaga.jsx
+++ src/pages/empresa/Cadastro_Vaga/cadastro_vaga.jsx
@@ -4,6 +4,7 @@ import "./cadastro_vaga.css";
 export const CadastrarVaga = () => {
     const [status, setStatus] = useState("");
     const [modalidade, setModalidade] = useState("");
+    const [descricao, setDescricao] = useState("");
     
   return (
     <div className="vaga-container">
@@ -55,6 +56,14 @@ export const CadastrarVaga = () => {
                   </select>
               </div>
         </div>
+        <div className="form-vaga-group">
+                  <textarea
+                      className="vaga-desc"
+                      placeholder="Descrição da vaga"
+                      value={descricao}
+                      onChange={(e) => setDescricao(e.target.value)}
+                  />
+              </div>
     </form>
   </div>
   );
