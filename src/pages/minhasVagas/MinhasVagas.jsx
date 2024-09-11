327a07f513d3acd412f6bd8dc890399a0d00b1c5
Juan Ramos <juanl.bnr@gmail.com>
2024-09-11 19:44:23 -0300

    ajustes na pagina de histórico de vagas do usuário
    * adicionado header e footer
    * trazendo as vagas do banco
    * e colocado o modal no onClick do botão visualizar.

--- src/pages/minhasVagas/MinhasVagas.jsx
+++ src/pages/minhasVagas/MinhasVagas.jsx
@@ -1,42 +1,93 @@
-import "./style.css"
-
+import { useEffect, useState } from "react";
+import Modal from 'react-modal';
+import { VagaServices } from "../../shared/infrastructure/services/VagaService";
+import { VagaDetail } from "../../shared/components/vagaDetail/VagaDetail"
+import { Footer, Header } from "../../shared/components";
 import { LiaLongArrowAltRightSolid } from "react-icons/lia";
+import "./style.css";
 
 export const MinhasVagas = () => {
+  const [vagas, setVagas] = useState([]);
+
+  const getVagas = async () => {
+    try {
+      const response = await VagaServices.getAll();
+      setVagas(response);
+    } catch (error) {
+      console.error("Erro ao buscar vagas:", error);
+    }
+  };
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedVaga, setSelectedVaga] = useState(null);
+
+  function openModal(vaga){
+    setSelectedVaga(vaga);
+    setModalIsOpen(true);
+};
+
+function closeModal(){
+    setModalIsOpen(false);
+    setSelectedVaga(null);
+};
+
+  useEffect(() => {
+    getVagas();
+  }, []);
 
   return (
+    <>
+    <Header />
     <div className="minhas-vagas-container">
-        <h1>Minhas vagas</h1>
-        <div className="vagas-lista">
-
-            {vagas.map(vaga => (
-
-            <div className="card-vaga">
-                <div className="status-data">
-                    <p  className={`status ${vaga.status}`}>{vaga.status}</p>
-                    <p>{vaga.dataInicio} - {vaga.dataFinal}</p>
-                </div>
-
-                <div className="titulo">
-                    <p>{vaga.titulo}</p>
-                </div>
-
-                <div className="nome-empresa">
-                    <p>{vaga.empresa}</p>
-                </div>
-
-                <div className="local">
-                    <p>{vaga.local}</p>
-                </div>
-
-                <div className="botao">
-                    <button className="botao-visualizar">Visualizar <LiaLongArrowAltRightSolid /></button>
-                </div>
+      <h1 className="font-bold text-2xl">Minhas vagas</h1>
+      <div className="vagas-lista">
+        {vagas.map((vaga) => (
+          <div key={vaga.uuid} className="card-vaga">
+            <div className="status-data">
+              <p className={`status ${vaga.status ? "Aberta" : "Fechada"}`}>
+                {vaga.status ? "Aberta" : "Fechada"}
+              </p>
+              <p>
+                {new Date(vaga.dataInicio).toLocaleDateString()} -{" "}
+                {new Date(vaga.dataFim).toLocaleDateString()}
+              </p>
             </div>
 
-            ))}
+            <div className="titulo">
+              <p>{vaga.titulo}</p>
+            </div>
 
-        </div>
+            <div className="nome-empresa">
+              <p>{vaga.empresa.nome}</p>
+            </div>
+
+            <div className="local">
+              <p>{vaga.cargo.nome}</p>
+            </div>
+
+            <div className="botao">
+              <button onClick={() => openModal(vaga)} className="flex items-center justify-between w-10 px-4 py-2 rounded tracking-wider bg-lite hover:bg-default-hover text-white text-[13px] cursor-pointer">
+                <p>Visualizar</p>
+                <LiaLongArrowAltRightSolid />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Detalhes da Vaga"
+        className="modal"
+        overlayClassName="modalBackground"
+      >
+        {selectedVaga && (
+            <VagaDetail vaga={selectedVaga} closeModal={closeModal} />
+        )}
+
+    </Modal>
     </div>
+    <Footer />
+    </>
   );
 };
--- src/shared/components/vagaDetail/VagaDetail.jsx
+++ src/shared/components/vagaDetail/VagaDetail.jsx
@@ -1,9 +1,7 @@
-import './style.css'
-
 import { Box } from '@mui/material';
-
 import { BsPencilSquare } from "react-icons/bs";
 import { IoClose } from "react-icons/io5";
+import './style.css'
 
 export const VagaDetail = ({ vaga, closeModal }) => {
     return (
@@ -14,17 +12,22 @@ export const VagaDetail = ({ vaga, closeModal }) => {
             </div>
 
             <h1>{vaga.titulo}</h1>
-            <p>{vaga.empresa}</p>
+            <p>{vaga.empresa.nome}</p>
             <div className='statusDiv'>
-                <p>{`${vaga.dataInicio} - ${vaga.dataFim}`}</p>
-                <p className={`status ${vaga.status}`}>{vaga.status}</p>
+              <p>
+                {new Date(vaga.dataInicio).toLocaleDateString()} -{" "}
+                {new Date(vaga.dataFim).toLocaleDateString()}
+              </p>
+                <p className={`status ${vaga.status ? "Aberta" : "Fechada"}`}>
+                  {vaga.status ? "Aberta" : "Fechada"}
+                </p>
             </div>
             <p>{vaga.local}</p>
             <h3>Sobre a vaga</h3>
             <p>{vaga.descricao}</p>
 
             <div className="modal-footer">
-                <button>
+                <button className="flex items-center justify-between w-10 px-4 py-2 rounded tracking-wider bg-lite hover:bg-default-hover text-white text-[13px] cursor-pointer">
                 <BsPencilSquare /> Inscrever-se
                 </button>
             </div>
