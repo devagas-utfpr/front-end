cefb378c28f095737e0f3802f96153806ff8e465
Eduardo Riki <eduardomatushita1@gmail.com>
2024-09-11 15:39:58 -0300

    fix: Correções gerais

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
     CadastrarEmpresa,
     ListagemVagas,
     MinhasVagas,
+    CadastrarVaga,
 } from "./pages";
 import {
     LOGIN,
@@ -11,15 +12,56 @@ import {
     CADASTRAR_EMPRESA,
     MINHASVAGAS,
     VAGAS,
+    CADASTRAR_VAGA,
 } from "./routes/routes";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { AuthProvider, useAuthContext } from "./shared/contexts/AuthContext";
+import { useEffect, useState } from "react";
 
-const Private = ({ children }) => {
-    const { isAuthenticated } = useAuthContext();
+const Private = () => {
+    const { isAuthenticated, isEmpresa } = useAuthContext();
+    const [loading, setLoading] = useState(true);
+
+    console.log("l", loading);
+    console.log("a", isAuthenticated);
+    console.log("i", isEmpresa);
+
+    useEffect(() => {
+        if (isAuthenticated !== undefined && isEmpresa !== undefined) {
+            setLoading(false);
+        }
+    }, [isAuthenticated, isEmpresa]);
+
+    if (loading) {
+        return <div>Loading...</div>; // Ou qualquer spinner de carregamento
+    }
 
     return isAuthenticated ? (
-        <>{children}</>
+        <>
+            {isEmpresa ? (
+                <Routes>
+                    <Route
+                        path={VAGAS}
+                        element={<ListagemVagas isEmpresa={isEmpresa} />}
+                    />
+                    <Route
+                        path={CADASTRAR_VAGA}
+                        element={<CadastrarVaga isEmpresa={isEmpresa} />}
+                    />
+                    <Route path="*" element={<Navigate to={VAGAS} />} />
+                </Routes>
+            ) : (
+                <Routes>
+                    <Route path={MINHASVAGAS} element={<MinhasVagas />} />
+                    <Route
+                        path={VAGAS}
+                        element={<ListagemVagas isEmpresa={isEmpresa} />}
+                    />
+                    <Route path={CADASTRAR_VAGA} element={<CadastrarVaga />} />
+                    <Route path="*" element={<Navigate to={VAGAS} />} />
+                </Routes>
+            )}
+        </>
     ) : (
         <Routes>
             <Route path={LOGIN} element={<Login />} />
@@ -34,13 +76,7 @@ export const App = () => {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <Private>
-                    <Routes>
-                        <Route path={MINHASVAGAS} element={<MinhasVagas />} />
-                        <Route path={VAGAS} element={<ListagemVagas />} />
-                        <Route path="*" element={<Navigate to={VAGAS} />} />
-                    </Routes>
-                </Private>
+                <Private />
             </AuthProvider>
         </BrowserRouter>
     );
