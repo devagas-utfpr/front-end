bdc48cd230ecf8216d4d61f61d853e191397b1c7
Eduardo Riki <eduardomatushita1@gmail.com>
2024-09-11 15:26:46 -0300

    fix: Correções gerais

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,3 +3,4 @@ export const CADASTRO = "/cadastrar-usuario";
 export const MINHASVAGAS = "/historico";
 export const CADASTRAR_EMPRESA = "/cadastrar-empresa";
 export const VAGAS = "/vagas";
+export const CADASTRAR_VAGA = "/cadastrar-vaga";
--- src/shared/infrastructure/index.js
+++ src/shared/infrastructure/index.js
@@ -1,1 +1,4 @@
 export * from "./services/UsuarioService";
+export * from "./services/EmpresaService";
+export * from "./services/VagaService";
+export * from "./services/CargoService";
